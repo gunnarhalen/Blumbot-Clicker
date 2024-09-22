4bd15ba282a6ef6ae7a12f8d82957fab6fd5b60a
Gunnar Halen <gunnar.halen@gmail.com>
2024-09-22 20:19:17 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,132 +1,50 @@
-# Blum Bot
+# Blumbot
 
-Automate the Blum Crypto game on your Telegram bot with Blum Bot! This Python tool allows the game to run autonomously, utilizing the pynput and keyboard modules. Designed for Python 3.11.6, it's easy to set up and get started.
+Blumbot é um bot automatizado desenvolvido em Python que interage com o aplicativo Telegram Desktop para realizar ações automáticas baseadas em detecção de cores e botões na interface. Este tutorial irá guiá-lo através do processo de instalação, configuração e uso do Blumbot.
 
-## Features
+## Índice
 
-- Automates the Blum Crypto game on Telegram.
-- Uses the `termcolor`,`pynput`, `pyautogui`, `pygetwindow` and `keyboard` modules for interaction.
-- Designed for Python 3.11.6.
-
-## Requirements
-
-- Python 3.11.6 (recommended)
-- `termcolor` module
-- `pyautogui` module
-- `pygetwindow` module
-- `pynput` module
-- `keyboard` module
-
-## Installation
-
-### Prerequisites
-
-Make sure you have Python 3.11.6 installed on your system. You can download it from the official [Python website](https://www.python.org/downloads/release/python-3116/).
-
-### Steps
-
-1. **Clone the repository**
-
-   ```sh
-   git clone https://github.com/DarkSkull777/blum-bot.git
-   cd blum-bot
-   ```
-
-2. **Install the required modules**
-
-   Install the necessary Python modules using `pip`:
-
-   ```sh
-   pip install termcolor
-   pip install colorama
-   pip install pynput
-   pip install keyboard
-   pip install pyautogui
-   pip install pygetwindow
-   ```
-
-3. **Run the script**
-
-   Execute the `blumbot.py` script:
-
-   ```sh
-   python blumbot.py
-   ```
-
-## Usage
-
-Once the script is running, it will automate the Blum Crypto game on your Telegram bot. Ensure your Telegram bot is properly set up and configured to work with the Blum Crypto game.
-
-## Contributing
-
-Contributions are welcome! Here's how you can contribute:
-
-1. **Fork the repository**
-
-   Click on the "Fork" button at the top right of this page to create a copy of this repository on your GitHub account.
-
-2. **Clone your forked repository**
-
-   ```sh
-   git clone https://github.com/DarkSkull777/blum-bot.git
-   cd blum-bot
-   ```
-
-3. **Create a new branch for your changes**
-
-   ```sh
-   git checkout -b my-new-feature
-   ```
-
-4. **Make your changes**
-
-   Modify the code and add your new features or improvements.
-
-5. **Commit your changes**
-
-   ```sh
-   git add .
-   git commit -m "Note"
-   ```
-
-6. **Push to your branch**
-
-   ```sh
-   git push origin your-new-feature
-   ```
-
-7. **Create a Pull Request**
-
-   Open a pull request from your forked repository to the original repository.
-
-### Suggestions and Stars
-
-If you have any suggestions or improvements, feel free to open an issue. If you like this repository and find it helpful, please give it a star ⭐!
-
-## License
-
-This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
-
-## Contact
-
-For any questions or support, please open an issue or contact the repository owner on [<span style="font-size:1.5em;">Telegram</span>](https://t.me/XSkull7).
+1. [Pré-requisitos](#pré-requisitos)
+2. [Instalação](#instalação)
+3. [Configuração](#configuração)
+4. [Uso](#uso)
 
 ---
 
-**Enjoying!**
+## Pré-requisitos
+
+Antes de começar, certifique-se de ter os seguintes itens instalados no seu sistema:
+
+- **Python 3.x**: Blumbot foi desenvolvido e testado com Python 3 (V3.10.6). Certifique-se de ter Python instalado.
+
+  - **Windows**: [Download Python](https://www.python.org/downloads/windows/)
+  - **macOS**: [Download Python](https://www.python.org/downloads/macos/)
+  - **Linux**: Utilize o gerenciador de pacotes da sua distribuição, por exemplo:
+
+    ```bash
+    sudo apt-get update
+    sudo apt-get install python3 python3-pip
+    ```
+
+- **Telegram Desktop**: Blumbot interage com a versão desktop do Telegram. [Download Telegram Desktop](https://desktop.telegram.org/)
 
 ---
 
-<details>
-<summary><strong>Table of Contents</strong></summary>
+## Instalação
 
-1. [Features](#features)
-2. [Requirements](#requirements)
-3. [Installation](#installation)
-4. [Download](#Download)
-5. [Usage](#usage)
-6. [Contributing](#contributing)
-7. [License](#license)
-8. [Contact](#contact)
+Siga os passos abaixo para instalar e configurar o Blumbot no seu sistema.
 
-</details>
+### 1. Clone o Repositório
+
+Primeiro, clone o repositório do Blumbot para o seu computador. Se você ainda não tem o Git instalado, [faça o download e instale o Git](https://git-scm.com/downloads).
+
+```bash
+git clone https://github.com/seu-usuario/blumbot.git
+```
+
+### 2. Abra diretório e execute o Blumbot
+
+Certifique-se de que o Blum está aberto antes de executar o bot. Abra o prompt de comando do seu Windows, navegue até a pasta do Blumbot e execute o comando abaixo:
+
+    python blumbot.py
+    
--- blumbot6.py
+++ blumbot6.py
@@ -1,7 +1,6 @@
 import pyautogui
 import pygetwindow as gw
 import time
-from termcolor import colored
 import keyboard
 import random
 from pynput.mouse import Button, Controller
@@ -13,33 +12,31 @@ time.sleep(0.5)
 
 def anjaymabar():
     print(f"""
-
 ░▒▓███████▓▒░░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓██████████████▓▒░░▒▓███████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░ 
 ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░     
 ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░     
 ░▒▓███████▓▒░░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░     
 ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░     
-░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░     
-░▒▓███████▓▒░░▒▓████████▓▒░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░  ░▒▓█▓▒░     
+░▒▓███████▓▒░░▒▓████████▓▒░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░  ░▒▓█▓▒░  
+  by Peralta   
     """)
 
 anjaymabar()
 print()
-print(":::::::::::::::::::: CHOOSE TELEGRAM LANGUAGE ::::::::::::::::::::")
-print("ENGLISH SELECTED BY DEFAULT")
+print(":::::::::::::::::::: ESCOLHA A JANELA DO TELEGRAM COM BLUM ::::::::::::::::::::")
 
 window_input = f"Choose Window (1 - TelegramDesktop): "
-window_not_found = f"Your Window - {{}} not found!"
-window_found = f"Window found - {{}}\n Bot working... Press 'K' on the keyboard to pause."
-pause_message = f"Bot paused... Press 'K' again on the keyboard to continue."
-continue_message = f"Bot continue working..."
+window_not_found = f"Janela {{}} não encontrada!"
+window_found = f"Janela encontrada: {{}}\n Iniciando bot... Aperte 'K' no teclado para pausar/reiniciar o Blumbot."
+pause_message = f"Blumbot pausado... Aperte 'K' novamente para continuar."
+continue_message = f"Blumbot reiniciado..."
 
 def click(x, y):
     mouse.position = (x, y + random.randint(1, 3))
     mouse.press(Button.left)
     mouse.release(Button.left)
 
-def locate_button(template_path):
+def locate_button(template_path, threshold=0.8):
     template = cv2.imread(template_path)
     template_height, template_width, _ = template.shape
 
@@ -48,23 +45,22 @@ def locate_button(template_path):
     scrn_np = cv2.cvtColor(scrn_np, cv2.COLOR_RGB2BGR)
 
     result = cv2.matchTemplate(scrn_np, template, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8
     loc = np.where(result >= threshold)
 
     if loc[0].size > 0:
         for pt in zip(*loc[::-1]):
             click(pt[0] + template_width // 2, pt[1] + template_height // 2)
+            pyautogui.moveTo(0, 0)  # Mover o mouse para fora da janela
             return True
 
     return False
 
 def scroll_window(window_rect):
-    # Move o mouse para o centro da janela antes de rolar
     center_x = window_rect[0] + window_rect[2] // 2
     center_y = window_rect[1] + window_rect[3] // 2
     mouse.position = (center_x, center_y)
     
-    for _ in range(5):  # Ajuste o número de rolagens conforme necessário
+    for _ in range(5):
         pyautogui.scroll(-100, x=center_x, y=center_y)
         time.sleep(0.2)
 
@@ -73,56 +69,50 @@ window_name = input(window_input)
 if window_name == '1':
     window_name = "TelegramDesktop"
 
-if window_name == '2':
-    window_name = "KotatogramDesktop"
-
 check = gw.getWindowsWithTitle(window_name)
 if not check:
     print(window_not_found.format(window_name))
-    print(f"EN: Make sure you use the TelegramDesktop application (not Telegram Web). And have opened the Blum bot on your TelegramDesktop. Until the Blum window is available.")
 else:
     print(window_found.format(window_name))
     telegram_window = check[0]
     paused = False
+    button_clicked = False
+    last_color_found_time = time.time()
 
     while True:
         if keyboard.is_pressed('K'):
             paused = not paused
-            if paused:
-                print(pause_message)
-            else:
-                print(continue_message)
+            print(pause_message if paused else continue_message)
             time.sleep(0.2)
 
         if paused:
             continue
 
-        # Tente ativar a janela do Telegram
         try:
             telegram_window.activate()
         except:
             telegram_window.minimize()
             telegram_window.restore()
 
-        window_rect = (
-            telegram_window.left, telegram_window.top, telegram_window.width, telegram_window.height
-        )
+        window_rect = (telegram_window.left, telegram_window.top, telegram_window.width, telegram_window.height)
 
-        # Rola a janela para trazer o botão para a vista
-        scroll_window(window_rect)
+        # Rola a janela e tenta localizar o botão Play
+        if not button_clicked:
+            scroll_window(window_rect)
 
-        # Localiza o botão antes de identificar cores
-        if not locate_button('images/playButton.png'):
-            print("Botão não encontrado. Aguardando...")
-            continue
+            if locate_button('images/playButton.png'):
+                print("Iniciando jogo.")
+                button_clicked = True
+            else:
+                print("Botão não encontrado. Aguardando...")
+                continue
 
+        # Loop de jogar clicando nas cores
         scrn = pyautogui.screenshot(region=(window_rect[0], window_rect[1], window_rect[2], window_rect[3]))
-
         width, height = scrn.size
         pixel_found = False
-        if pixel_found:
-            break
 
+        # LEMBRETE: Testar range com valor 10 para aumento do performance na identificação das cores. Pode aumentar uso de CPU.
         for x in range(0, width, 20):
             for y in range(0, height, 20):
                 r, g, b = scrn.getpixel((x, y))
@@ -130,6 +120,21 @@ else:
                     screen_x = window_rect[0] + x
                     screen_y = window_rect[1] + y
                     click(screen_x + 4, screen_y)
-                    time.sleep(0.001)
+                    last_color_found_time = time.time()
+                    # LEMBRETE: Testar time.sleep(0.0001) para evitar clique errado na bomba
+                    time.sleep(0.001) 
                     pixel_found = True
                     break
+
+        # Se passaram mais de 3 segundos sem encontrar cor, reinicia o jogo
+        if not pixel_found and (time.time() - last_color_found_time > 3):
+            print("Fim de partida. Reiniciando jogo.")
+            
+            if locate_button('images/playAgainButton.png', threshold=0.6):
+                print("'Jogando novamente'.")
+                button_clicked = True  # Continua jogando após clicar no "Play Again"
+                last_color_found_time = time.time()
+                time.sleep(1)  # Pequeno atraso para garantir que o jogo reinicie
+            else:
+                print("Sem fichas restantes. Encerrando o Blumbot...")
+                break  # Encerra o loop se o botão não for encontrado
